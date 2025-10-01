225d8008e780cea56e870a8869ce9eac3860e4f6
dartrix501 <luinav2007@gmail.com>
2025-10-01 18:55:27 -0500

    Prueba de MongoDB

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
 
 app = FastAPI()
 
 origins = ["https://testeos-chi.vercel.app"]
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["mi_app"]
+coleccion = db["contactos"]
+
 
 # Permitir peticiones desde cualquier origen (Vercel incluido)
 app.add_middleware(
@@ -29,5 +39,10 @@ def procesar(contacto: Contacto):
     mensaje = {"mensaje": f"Datos recibidos correctamente: {contacto.nombre}, {contacto.email}"}
     print(mensaje)
     return mensaje
+
+@app.get("/contactos")
+async def obtener_contactos():
+    contactos = await coleccion.find().to_list(100)
+    return contactos
 
 print("Hello world")
